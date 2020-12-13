d404c4370aa1f4f9326086e243b6bdbbab6956cb
Louis Cordier <lcordier@gmail.com>
2020-12-13 23:32:17 +0200

    Implement m4b_split.py.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # audio_tools
+
+Tool to convert audiobooks into easy digestable mp3s for my mp3 player.
+
+
+## Requirements
+
+* [FFmpeg](https://ffmpeg.org/)
+* [mp3splt](https://github.com/mp3splt/mp3splt)
+
---
+++ m4b_split.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+""" Tool to convert audiobooks into easy digestable mp3s for my mp3 player.
+
+    sudo apt install ffmpeg mp3splt
+"""
+import glob
+import optparse
+import os
+import subprocess
+
+
+if __name__ == '__main__':
+
+    parser = optparse.OptionParser('%prog [options] file1.m4b [file2.m4b ...]')
+
+    parser.add_option('-o',
+                      '--output',
+                      dest='prefix',
+                      action='store',
+                      type='string',
+                      default='output',
+                      help='output prefix [output]')
+
+    parser.add_option('-t',
+                      '--time',
+                      dest='time',
+                      action='store',
+                      type='string',
+                      default='25.00',
+                      help='time segment [mm.ss], [25.00]')
+
+    parser.add_option('-k',
+                      '--keep',
+                      dest='keep',
+                      action='store_true',
+                      default=False,
+                      help='keep work-in-progress files')
+
+    options, args = parser.parse_args()
+
+    prefix = options.prefix
+    time = options.time
+    keep = options.keep
+
+    # Convert .m4b to .mp3
+    for idx, filename in enumerate(args, 1):
+        command = f'ffmpeg -i "{filename}" -acodec libmp3lame -ar 22050 "{prefix}_{idx:02d}.mp3"'
+        try:
+            subprocess.check_call(command, shell=True)
+        except subprocess.CalledProcessError:
+            pass
+
+    # Split .mp3s
+    inputs = glob.glob(f'{prefix}_??.mp3')
+    for input in inputs:
+        command = f'mp3splt -t "{time}" -a -d "{prefix}" -o "@f_@n" "{input}"'
+        subprocess.check_call(command, shell=True)
+
+        if not keep:
+            os.remove(input)
+
